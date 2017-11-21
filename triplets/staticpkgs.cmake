5df4f57344d77c75fb95d7a1ce2cfaccbe0960f7
Ryan Pavlik <ryan@sensics.com>
2017-11-21 10:51:14 +0100

    Adjust list of static packages

--- triplets/staticpkgs.cmake
+++ triplets/staticpkgs.cmake
@@ -1,6 +1,8 @@
 set(STATIC_PACKAGES
-	jsoncpp
-	protobuf)
+	jsoncpp # Simpler and easier this way
+	protobuf # Build errors if dynamic?
+	zlib # since we aren't using the official zlib binaries, they prefer static linking...
+	)
 
 foreach(PKG ${STATIC_PACKAGES})
 	if(PORT STREQUAL PKG)
